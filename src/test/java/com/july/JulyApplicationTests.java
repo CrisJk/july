08f60393db4d8975a48540d76d5bc3947546c7da
kj <kjmd2d@sina.cn>
2016-05-05 10:18:35 +0800

    init

---
+++ src/test/java/com/july/JulyApplicationTests.java
@@ -0,0 +1,18 @@
+package com.july;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = JulyApplication.class)
+@WebAppConfiguration
+public class JulyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
